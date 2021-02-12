9883cbfaac504b2fe19e8e4d6fdc37ac04af907c
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-02-11 18:06:41 -0600

    fixed

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -2,14 +2,15 @@ const $pizzaList = document.querySelector('#pizza-list');
 
 const getPizzaList = () => {
   fetch('/api/pizzas')
-  .then(response => response.json())
-  .then(pizzaListArr => {
-    pizzaListArr.forEach(printPizza);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(response => response.json())
+    .then(pizzaListArr => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
+
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
     <div class="col-12 col-lg-6 flex-row">
@@ -35,8 +36,6 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
   `;
 
   $pizzaList.innerHTML += pizzaCard;
-  
 };
-
 
 getPizzaList();
